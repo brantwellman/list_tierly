c28d936a94409f00ea7d350c8d9815e070658d65
Brant Wellman <brantwellman@gmail.com>
2025-04-22 10:24:06 -0600

    Temporarily set current_user based on seeds, until authentication implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Temporary way to set current_user until authentication is added
+  def current_user
+    @current_user ||= User.first || raise("No user found")
+  end
 end
